fb745a01340e6edf48ba1fd9a68d1c666b5dc5e1
Nicholas <bestnicholas05@gmail.com>
2024-05-14 21:34:02 -0400

    Completed Array Filter Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, element){
+    return drivers.filter(nametofilter => nametofilter.toLowerCase() === element.toLowerCase())
+  }
+ 
+function fuzzyMatch(drivers, startingLetter){
+    return drivers.filter(driver => driver.startsWith(startingLetter))
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+
